2e12cdc0dbc0ab2019a3e793e7f8e12026beaa7e
Pritish Shamkuwar <pritishshamkuwar8888@gmail.com>
2020-05-04 11:18:46 +0700

    create a add button in recycle view and delete icon, function not complete yet

--- Gumi Vietnam/app/src/main/java/com/example/gumivietnam/MyAdapter.kt
+++ Gumi Vietnam/app/src/main/java/com/example/gumivietnam/MyAdapter.kt
@@ -1,40 +1,50 @@
 package com.example.gumivietnam
 
 import android.content.Context
-import android.icu.text.CaseMap
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
+import java.util.*
 
-class MyAdapter (
+
+class MyAdapter(
     private val context: Context,
     private val model: List<Model>,
-    val listener : (Model) -> Unit
-): RecyclerView.Adapter<MyAdapter.ModelViewHolder>() {
-    class ModelViewHolder(view: View) : RecyclerView.ViewHolder(view){
+    private val mRandom: Random = Random(),
+    val listener: (Model) -> Unit
+) :
+    RecyclerView.Adapter<MyAdapter.ModelViewHolder>()
+{
+
+    class ModelViewHolder(view: View) : RecyclerView.ViewHolder(view)
+    {
         val image = view.findViewById<ImageView>(R.id.red)
         val title = view.findViewById<TextView>(R.id.company)
         val descriptor = view.findViewById<TextView>(R.id.take)
-        fun bindView(model: Model, listener: (Model) -> Unit){
+        val delete = view.findViewById<ImageView>(R.id.imageDelete)
+        fun bindView(model: Model, listener: (Model) -> Unit)
+        {
             image.setImageResource(model.image)
             title.text = model.title
             descriptor.text = model.descriptor
             itemView.setOnClickListener { listener(model) }
+
         }
+
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ModelViewHolder
-            = ModelViewHolder(LayoutInflater.from(context).inflate(R.layout.row, parent, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ModelViewHolder =
+        ModelViewHolder(LayoutInflater.from(context).inflate(R.layout.row, parent, false))
 
     override fun getItemCount(): Int = model.size
 
 
-    override fun onBindViewHolder(holder: ModelViewHolder, position: Int) {
-        holder.bindView(model [position],listener )
+    override fun onBindViewHolder(holder: ModelViewHolder, position: Int)
+    {
+        holder.bindView(model[position], listener)
+
     }
 }
--- Gumi Vietnam/app/src/main/java/com/example/gumivietnam/ShowListActivity.kt
+++ Gumi Vietnam/app/src/main/java/com/example/gumivietnam/ShowListActivity.kt
@@ -4,21 +4,32 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.renderscript.ScriptGroup
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.AdapterView
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_show_list.*
+import kotlinx.android.synthetic.main.add_message_dialog.view.*
+import kotlinx.android.synthetic.main.row.*
+import java.util.*
 
 class ShowListActivity : AppCompatActivity() {
     companion object{
         val INTENT_PARCELABLE = "OBJECT_INTENT"
     }
+    private val mRandom = Random()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_list)
+
+        this.title = "Messages"
 
         val modelList = listOf<Model>(
             Model("Notification From Company", "Take a day off", R.drawable.email),
@@ -43,6 +54,37 @@ class ShowListActivity : AppCompatActivity() {
             intent.putExtra(INTENT_PARCELABLE, it)
             startActivity(intent)
         }
+
+
+        btn.setOnClickListener {
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.add_message_dialog, null)
+
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("Add Message")
+
+            val mAlertDialog = mBuilder.show()
+
+            mDialogView.add.setOnClickListener {
+
+                mAlertDialog.dismiss()
+
+                val message = mDialogView.add.editText.toString()
+                val des = mDialogView.des.editText.toString()
+
+            }
+
+            mDialogView.cancel_button.setOnClickListener {
+                mAlertDialog.dismiss()
+            }
+            mDialogView.add_btn.setOnClickListener {
+
+                Toast.makeText(this@ShowListActivity, " You want To add message", Toast.LENGTH_LONG).show()
+            }
+        }
+
     }
 }
+
+
 
